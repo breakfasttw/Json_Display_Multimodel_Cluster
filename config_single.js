f2f90b0b68197eeebf41e0b98cc2a7c728986358
breakfast.tw <tiffanyw.richi@gmail.com>
2026-05-21 16:33:08 +0800

    add version data

--- config_single.js
+++ config_single.js
@@ -2,39 +2,79 @@
 const singleFileDescribe = [
     {
         fileName: "khshu_-20251228133052-1388935893035804.json",
-        fileDisplayName: "千千 - 聖誕樹頭飾",
+        fileDisplayName: "千千 - 聖誕樹頭飾-v11",
         url: "./data/single/khshu_-20251228133052-1388935893035804.json",
         video_url: "",
-        structure_version: "json_structure_02.json",
+        structure_version: "json_structure_11.json",
     },
     {
         fileName: "wia627-20250212084030-1642331380410258.json",
-        fileDisplayName: "上發條俱樂部 - 5歲生日",
+        fileDisplayName: "上發條俱樂部 - 5歲生日-v11",
         url: "./data/single/wia627-20250212084030-1642331380410258.json",
         video_url: "https://www.youtube.com/watch?v=lwHF4Gf74Ho",
-        structure_version: "json_structure_02.json", // 指派新的結構檔
+        structure_version: "json_structure_11.json", // 指派新的結構檔
     },
     {
         fileName: "walkerdad1228-20250923040624-25870566612599789-03.json",
-        fileDisplayName: "5個小女孩跳 K-POP 舞蹈",
+        fileDisplayName: "5個小女孩跳 K-POP 舞蹈-v11",
         url: "./data/single/walkerdad1228-20250923040624-25870566612599789-03.json",
         video_url: "",
-        structure_version: "json_structure_02.json", // 指派新的結構檔
+        structure_version: "json_structure_11.json", // 指派新的結構檔
     },
 
     {
         fileName: "rayduenglish-20250804150549-1559856595225419.json",
-        fileDisplayName: "阿滴輔大學餐",
+        fileDisplayName: "阿滴輔大學餐-v11",
         url: "./data/single/rayduenglish-20250804150549-1559856595225419.json",
         video_url: "",
-        structure_version: "json_structure_02.json", // 指派新的結構檔
+        structure_version: "json_structure_11.json", // 指派新的結構檔
     },
 
     {
         fileName: "20141010hero-20250730221253-1464377325322334.json",
-        fileDisplayName: "媽祖繞境",
+        fileDisplayName: "媽祖繞境-v11",
         url: "./data/single/20141010hero-20250730221253-1464377325322334.json",
         video_url: "",
-        structure_version: "json_structure_02.json", // 指派新的結構檔
+        structure_version: "json_structure_11.json", // 指派新的結構檔
+    },
+
+    {
+        fileName: "wia627-20250212084030-1642331380410258-v12.json",
+        fileDisplayName: "上發條俱樂部 - 5歲生日-v12-1",
+        url: "./data/single/wia627-20250212084030-1642331380410258-v12.json",
+        video_url: "",
+        structure_version: "json_structure_12-1.json", // 指派新的結構檔
+    },
+
+    {
+        fileName: "rayduenglish-20250521032320-1519278952471525.json",
+        fileDisplayName: "阿滴業配潔美淨 -v12-1",
+        url: "./data/single/rayduenglish-20250521032320-1519278952471525.json",
+        video_url: "",
+        structure_version: "json_structure_12-1.json", // 指派新的結構檔
+    },
+
+    {
+        fileName: "japanuts-20251006112634-2903944809795619.json",
+        fileDisplayName: "japanuts旅遊無旁白 -v12-2",
+        url: "./data/single/japanuts-20251006112634-2903944809795619.json",
+        video_url: "",
+        structure_version: "json_structure_12-2.json", // 指派新的結構檔
+    },
+
+    {
+        fileName: "walkerdad1228-20250923040624-25870566612599789-v12.json",
+        fileDisplayName: "五個小女孩跳舞 -v12-3",
+        url: "./data/single/walkerdad1228-20250923040624-25870566612599789-v12.json",
+        video_url: "",
+        structure_version: "json_structure_12-3.json", // 指派新的結構檔
+    },
+
+    {
+        fileName: "rayduenglish-20250521032320-1519278952471525-v12-3.json",
+        fileDisplayName: "阿滴業配潔美淨 -v12-3",
+        url: "./data/single/rayduenglish-20250521032320-1519278952471525-v12-3.json",
+        video_url: "",
+        structure_version: "json_structure_12-3.json", // 指派新的結構檔
     },
 ];
