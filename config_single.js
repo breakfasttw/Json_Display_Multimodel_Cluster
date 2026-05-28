6833cc5ea3ef9147f85560acbbfcf9056e303cf1
breakfast.tw <tiffanyw.richi@gmail.com>
2026-05-28 17:16:39 +0800

    update to v19

--- config_single.js
+++ config_single.js
@@ -1,83 +1,5 @@
 // Single 模式的檔案設定
 const singleFileDescribe = [
-    // {
-    //     fileName: "khshu_-20251228133052-1388935893035804.json",
-    //     fileDisplayName: "千千 - 聖誕樹頭飾-v11",
-    //     url: "./data/single/khshu_-20251228133052-1388935893035804.json",
-    //     video_url: "",
-    //     structure_version: "json_structure_11.json",
-    // },
-    // {
-    //     fileName: "wia627-20250212084030-1642331380410258.json",
-    //     fileDisplayName: "上發條俱樂部 - 5歲生日-v11",
-    //     url: "./data/single/wia627-20250212084030-1642331380410258.json",
-    //     video_url: "https://www.youtube.com/watch?v=lwHF4Gf74Ho",
-    //     structure_version: "json_structure_11.json", // 指派新的結構檔
-    // },
-    // {
-    //     fileName: "walkerdad1228-20250923040624-25870566612599789-03.json",
-    //     fileDisplayName: "5個小女孩跳 K-POP 舞蹈-v11",
-    //     url: "./data/single/walkerdad1228-20250923040624-25870566612599789-03.json",
-    //     video_url: "",
-    //     structure_version: "json_structure_11.json", // 指派新的結構檔
-    // },
-
-    // {
-    //     fileName: "rayduenglish-20250804150549-1559856595225419.json",
-    //     fileDisplayName: "阿滴輔大學餐-v11",
-    //     url: "./data/single/rayduenglish-20250804150549-1559856595225419.json",
-    //     video_url: "",
-    //     structure_version: "json_structure_11.json", // 指派新的結構檔
-    // },
-
-    // {
-    //     fileName: "20141010hero-20250730221253-1464377325322334.json",
-    //     fileDisplayName: "媽祖繞境-v11",
-    //     url: "./data/single/20141010hero-20250730221253-1464377325322334.json",
-    //     video_url: "",
-    //     structure_version: "json_structure_11.json", // 指派新的結構檔
-    // },
-
-    // {
-    //     fileName: "wia627-20250212084030-1642331380410258-v12.json",
-    //     fileDisplayName: "上發條俱樂部 - 5歲生日-v12-1",
-    //     url: "./data/single/wia627-20250212084030-1642331380410258-v12.json",
-    //     video_url: "",
-    //     structure_version: "json_structure_12-1.json", // 指派新的結構檔
-    // },
-
-    // {
-    //     fileName: "rayduenglish-20250521032320-1519278952471525.json",
-    //     fileDisplayName: "阿滴業配潔美淨 -v12-1",
-    //     url: "./data/single/rayduenglish-20250521032320-1519278952471525.json",
-    //     video_url: "",
-    //     structure_version: "json_structure_12-1.json", // 指派新的結構檔
-    // },
-
-    // {
-    //     fileName: "japanuts-20251006112634-2903944809795619.json",
-    //     fileDisplayName: "japanuts旅遊無旁白 -v12-2",
-    //     url: "./data/single/japanuts-20251006112634-2903944809795619.json",
-    //     video_url: "",
-    //     structure_version: "json_structure_12-2.json", // 指派新的結構檔
-    // },
-
-    // {
-    //     fileName: "walkerdad1228-20250923040624-25870566612599789-v12.json",
-    //     fileDisplayName: "五個小女孩跳舞 -v12-3",
-    //     url: "./data/single/walkerdad1228-20250923040624-25870566612599789-v12.json",
-    //     video_url: "",
-    //     structure_version: "json_structure_12-3.json", // 指派新的結構檔
-    // },
-
-    // {
-    //     fileName: "rayduenglish-20250521032320-1519278952471525-v12-3.json",
-    //     fileDisplayName: "阿滴業配潔美淨 -v12-3",
-    //     url: "./data/single/rayduenglish-20250521032320-1519278952471525-v12-3.json",
-    //     video_url: "",
-    //     structure_version: "json_structure_12-3.json", // 指派新的結構檔
-    // },
-
     {
         fileName: "悲傷樂樂-v13.json",
         fileDisplayName: "[短片 audio 測試] 悲傷樂樂-v13",
@@ -150,68 +72,267 @@ const singleFileDescribe = [
         structure_version: "json_structure_13.json", // 指派新的結構檔
     },
 
-    {
-        fileName: "wia627-20250212084030-1642331380410258-v14.json",
-        fileDisplayName: "上發條俱樂部 -幫小孩慶生 (有貓叫、多場景) -v14",
-        url: "./data/single/wia627-20250212084030-1642331380410258-v14.json",
-        video_url: "https://www.instagram.com/reel/DFQbT3mzwSA",
-        structure_version: "json_structure_14.json", // 指派新的結構檔
-    },
+    // {
+    //     fileName: "wia627-20250212084030-1642331380410258-v14.json",
+    //     fileDisplayName: "上發條俱樂部 -幫小孩慶生 (有貓叫、多場景) -v14",
+    //     url: "./data/single/wia627-20250212084030-1642331380410258-v14.json",
+    //     video_url: "https://www.instagram.com/reel/DFQbT3mzwSA",
+    //     structure_version: "json_structure_14.json", // 指派新的結構檔
+    // },
+
+    // {
+    //     fileName: "20141010hero-20250730221253-1464377325322334-v14.json",
+    //     fileDisplayName: "英雄日常 - 媽祖繞境 (虔誠、背景雜音) -v14",
+    //     url: "./data/single/20141010hero-20250730221253-1464377325322334-v14.json",
+    //     video_url: "https://www.instagram.com/reel/DJQjELQvXYu",
+    //     structure_version: "json_structure_14.json", // 指派新的結構檔
+    // },
+
+    // {
+    //     fileName: "chuchumei__-20250805105548-1276250431031515-v14.json",
+    //     fileDisplayName: "啾啾妹 -  2D 圖文創作 (無 BGM) -14",
+    //     url: "./data/single/chuchumei__-20250805105548-1276250431031515-v14.json",
+    //     video_url: "",
+    //     structure_version: "json_structure_14.json", // 指派新的結構檔
+    // },
+
+    // {
+    //     fileName: "joemanweng-20251128120717-1943369030398032-v14.json",
+    //     fileDisplayName: "Joeman -  快節奏空拍旅遊 (bgm) -v14",
+    //     url: "./data/single/joemanweng-20251128120717-1943369030398032-v14.json",
+    //     video_url: "https://www.instagram.com/reel/DRebfF7iVoe",
+    //     structure_version: "json_structure_14.json", // 指派新的結構檔
+    // },
+
+    // {
+    //     fileName: "rayduenglish-20250804150549-1559856595225419-v14.json",
+    //     fileDisplayName: "阿滴 - 逛輔大校園嗆 Joeman (有背景雜訊)-v14",
+    //     url: "./data/single/rayduenglish-20250804150549-1559856595225419-v14.json",
+    //     video_url: "https://www.instagram.com/reel/DH-GKiygHyU",
+    //     structure_version: "json_structure_14.json", // 指派新的結構檔
+    // },
+
+    // {
+    //     fileName: "walkerdad1228-20250923040624-25870566612599789-v14.json",
+    //     fileDisplayName:
+    //         "喪屍老爸 - 五個小女孩跳 k-pop 舞蹈 (純 bgm、對嘴唱歌) v-14",
+    //     url: "./data/single/walkerdad1228-20250923040624-25870566612599789-v14.json",
+    //     video_url: "https://www.instagram.com/reel/DGKAqzdsl-k",
+    //     structure_version: "json_structure_14.json", // 指派新的結構檔
+    // },
+
+    // {
+    //     fileName: "wufeili-20250709125850-26299444203001810-v14.json",
+    //     fileDisplayName: "吳斐莉 - 瑞士旅遊 (緩慢敘述、自然噪音) -v14",
+    //     url: "./data/single/wufeili-20250709125850-26299444203001810-v14.json",
+    //     video_url: "https://www.instagram.com/reel/DLfLXlrykQe",
+    //     structure_version: "json_structure_14.json", // 指派新的結構檔
+    // },
+
+    // {
+    //     fileName: "zoebitalk-20250101212122-1966008960997105-v14.json",
+    //     fileDisplayName: "肉比頭 - 看落羽松溫暖旅遊 (輕柔 bgm、和緩旁白) -v14",
+    //     url: "./data/single/zoebitalk-20250101212122-1966008960997105-v14.json",
+    //     video_url: "https://www.instagram.com/reel/DER2K4QpHfe",
+    //     structure_version: "json_structure_14.json", // 指派新的結構檔
+    // },
+
+    // {
+    //     fileName: "joemanweng-20251128120717-1943369030398032-v15.json",
+    //     fileDisplayName: "Joeman -  快節奏空拍旅遊 (bgm) -v15",
+    //     url: "./data/single/joemanweng-20251128120717-1943369030398032-v15.json",
+    //     video_url: "",
+    //     structure_version: "json_structure_15.json", // 指派新的結構檔
+    // },
+
+    // {
+    //     fileName: "20141010hero-20250730221253-1464377325322334-v15.json",
+    //     fileDisplayName: "英雄日常 - 媽祖繞境 (虔誠、背景雜音) -v15",
+    //     url: "./data/single/20141010hero-20250730221253-1464377325322334-v15.json",
+    //     video_url: "",
+    //     structure_version: "json_structure_15.json", // 指派新的結構檔
+    // },
+
+    // {
+    //     fileName: "cocowine0205-20250217100535-1617257429400233-v15.json",
+    //     fileDisplayName: "可可酒精 - 比爾好市多週報 -v15",
+    //     url: "./data/single/cocowine0205-20250217100535-1617257429400233-v15.json",
+    //     video_url: "",
+    //     structure_version: "json_structure_15.json", // 指派新的結構檔
+    // },
+
+    // {
+    //     fileName: "cocowine0205-20251211195141-1476024013877212-v15.json",
+    //     fileDisplayName: "可可酒精 - 好市多線上購葉黃素業配 -v15",
+    //     url: "./data/single/cocowine0205-20251211195141-1476024013877212-v15.json",
+    //     video_url: "",
+    //     structure_version: "json_structure_15.json", // 指派新的結構檔
+    // },
+
+    // {
+    //     fileName: "zoebitalk-20250101212122-1966008960997105-v15.json",
+    //     fileDisplayName: "肉比頭 - 看落羽松溫暖旅遊 (輕柔 bgm、和緩旁白) -v15",
+    //     url: "./data/single/zoebitalk-20250101212122-1966008960997105-v15.json",
+    //     video_url: "",
+    //     structure_version: "json_structure_15.json", // 指派新的結構檔
+    // },
+
+    // {
+    //     fileName: "20141010hero-20250730221253-1464377325322334-v16.json",
+    //     fileDisplayName: "英雄日常 - 媽祖繞境 (虔誠、背景雜音) -v16",
+    //     url: "./data/single/20141010hero-20250730221253-1464377325322334-v16.json",
+    //     video_url: "",
+    //     structure_version: "json_structure_16.json", // 指派新的結構檔
+    // },
+
+    // {
+    //     fileName: "zoebitalk-20250101212122-1966008960997105-v16.json",
+    //     fileDisplayName: "肉比頭 - 看落羽松溫暖旅遊 (輕柔 bgm、和緩旁白) -v16",
+    //     url: "./data/single/zoebitalk-20250101212122-1966008960997105-v16.json",
+    //     video_url: "",
+    //     structure_version: "json_structure_16.json", // 指派新的結構檔
+    // },
+
+    // {
+    //     fileName: "rayduenglish-20250804150549-1559856595225419-v16.json",
+    //     fileDisplayName: "阿滴 - 逛輔大校園嗆 Joeman (有背景雜訊和 SFX) -v16",
+    //     url: "./data/single/rayduenglish-20250804150549-1559856595225419-v16.json",
+    //     video_url: "",
+    //     structure_version: "json_structure_16.json", // 指派新的結構檔
+    // },
 
     {
-        fileName: "20141010hero-20250730221253-1464377325322334-v14.json",
-        fileDisplayName: "英雄日常 - 媽祖繞境 (虔誠、背景雜音) -v14",
-        url: "./data/single/20141010hero-20250730221253-1464377325322334-v14.json",
-        video_url: "https://www.instagram.com/reel/DJQjELQvXYu",
-        structure_version: "json_structure_14.json", // 指派新的結構檔
-    },
-
-    {
-        fileName: "chuchumei__-20250805105548-1276250431031515-v14.json",
-        fileDisplayName: "啾啾妹 -  2D 圖文創作 (無 BGM) -14",
-        url: "./data/single/chuchumei__-20250805105548-1276250431031515-v14.json",
+        fileName: "20141010hero-20250730221253-1464377325322334-v17.json",
+        fileDisplayName: "英雄日常 - 媽祖繞境 (虔誠、背景雜音、鞭炮) -v17",
+        url: "./data/single/20141010hero-20250730221253-1464377325322334-v17.json",
         video_url: "",
-        structure_version: "json_structure_14.json", // 指派新的結構檔
+        structure_version: "json_structure_17.json", // 指派新的結構檔
+    },
+    {
+        fileName: "zoebitalk-20250101212122-1966008960997105-v17.json",
+        fileDisplayName: "肉比頭 - 看落羽松溫暖旅遊 (輕柔 bgm、和緩旁白) -v17",
+        url: "./data/single/zoebitalk-20250101212122-1966008960997105-v17.json",
+        video_url: "",
+        structure_version: "json_structure_17.json", // 指派新的結構檔
     },
 
     {
-        fileName: "joemanweng-20251128120717-1943369030398032-v14.json",
-        fileDisplayName: "Joeman -  快節奏空拍旅遊 (bgm) -v14",
-        url: "./data/single/joemanweng-20251128120717-1943369030398032-v14.json",
-        video_url: "https://www.instagram.com/reel/DRebfF7iVoe",
-        structure_version: "json_structure_14.json", // 指派新的結構檔
+        fileName: "wia627-20250212084030-1642331380410258-v17.json",
+        fileDisplayName: "上發條俱樂部 -幫小孩慶生 (有貓叫、多場景) -v17",
+        url: "./data/single/wia627-20250212084030-1642331380410258-v17.json",
+        video_url: "https://www.instagram.com/reel/DFQbT3mzwSA",
+        structure_version: "json_structure_17.json", // 指派新的結構檔
     },
 
     {
-        fileName: "rayduenglish-20250804150549-1559856595225419-v14.json",
-        fileDisplayName: "阿滴 - 逛輔大校園嗆 Joeman (有背景雜訊)-v14",
-        url: "./data/single/rayduenglish-20250804150549-1559856595225419-v14.json",
-        video_url: "https://www.instagram.com/reel/DH-GKiygHyU",
-        structure_version: "json_structure_14.json", // 指派新的結構檔
+        fileName: "cocowine0205-20251211195141-1476024013877212-v17.json",
+        fileDisplayName: "可可酒精 - 好市多線上購葉黃素業配 -v17",
+        url: "./data/single/cocowine0205-20251211195141-1476024013877212-v17.json",
+        video_url: "",
+        structure_version: "json_structure_17.json", // 指派新的結構檔
     },
 
     {
-        fileName: "walkerdad1228-20250923040624-25870566612599789-v14.json",
-        fileDisplayName:
-            "喪屍老爸 - 五個小女孩跳 k-pop 舞蹈 (純 bgm、對嘴唱歌) v-14",
-        url: "./data/single/walkerdad1228-20250923040624-25870566612599789-v14.json",
-        video_url: "https://www.instagram.com/reel/DGKAqzdsl-k",
-        structure_version: "json_structure_14.json", // 指派新的結構檔
+        fileName: "1_shiuan_0-20251001001108-3776936692602884-v18.json",
+        fileDisplayName: "林瑄 - 自拍生活 vlog -v18",
+        url: "./data/single/1_shiuan_0-20251001001108-3776936692602884-v18.json",
+        video_url: "",
+        structure_version: "json_structure_18.json", // 指派新的結構檔
     },
 
     {
-        fileName: "wufeili-20250709125850-26299444203001810-v14.json",
-        fileDisplayName: "吳斐莉 - 瑞士旅遊 (緩慢敘述、自然噪音) -v14",
-        url: "./data/single/wufeili-20250709125850-26299444203001810-v14.json",
-        video_url: "https://www.instagram.com/reel/DLfLXlrykQe",
-        structure_version: "json_structure_14.json", // 指派新的結構檔
+        fileName: "peeta.gege-20251225231707-2312044362627471-v18.json",
+        fileDisplayName: "peeta葛格- 業配高蛋白 -v18",
+        url: "./data/single/peeta.gege-20251225231707-2312044362627471-v18.json",
+        video_url: "",
+        structure_version: "json_structure_18.json", // 指派新的結構檔
     },
 
     {
-        fileName: "zoebitalk-20250101212122-1966008960997105-v14.json",
-        fileDisplayName: "肉比頭 - 看落羽松溫暖旅遊 (輕柔 bgm、和緩旁白) -v14",
-        url: "./data/single/zoebitalk-20250101212122-1966008960997105-v14.json",
-        video_url: "https://www.instagram.com/reel/DER2K4QpHfe",
-        structure_version: "json_structure_14.json", // 指派新的結構檔
+        fileName: "zxsdexz-20250304201403-809756348802886-v18.json",
+        fileDisplayName: "黃帝- 釣蝦 -v18",
+        url: "./data/single/zxsdexz-20250304201403-809756348802886-v18.json",
+        video_url: "",
+        structure_version: "json_structure_18.json", // 指派新的結構檔
+    },
+
+    {
+        fileName: "wia627-20250212084030-1642331380410258-v18.json",
+        fileDisplayName: "上發條俱樂部 -幫小孩慶生 (有貓叫、多場景) -v18",
+        url: "./data/single/wia627-20250212084030-1642331380410258-v18.json",
+        video_url: "",
+        structure_version: "json_structure_18.json", // 指派新的結構檔
+    },
+
+    {
+        fileName: "khshu_-20250806235022-1411343207297987-v18.json",
+        fileDisplayName: "千千 - 業配全家仙草冰淇淋 -v18",
+        url: "./data/single/khshu_-20250806235022-1411343207297987-v18.json",
+        video_url: "",
+        structure_version: "json_structure_18.json", // 指派新的結構檔
+    },
+
+    {
+        fileName: "中國群眾激昂-v18.json",
+        fileDisplayName: "中國群眾激昂嘶吼 -v18",
+        url: "./data/single/中國群眾激昂-v18.json",
+        video_url: "",
+        structure_version: "json_structure_18.json", // 指派新的結構檔
+    },
+
+    {
+        fileName: "蔡波能雲霄飛車尖叫-v18.json",
+        fileDisplayName: "蔡波能 - 和二伯雲霄飛車尖叫 - v18",
+        url: "./data/single/蔡波能雲霄飛車尖叫-v18.json",
+        video_url: "",
+        structure_version: "json_structure_18.json", // 指派新的結構檔
+    },
+
+    {
+        fileName: "悲傷樂樂-v18.json",
+        fileDisplayName: "悲傷樂樂 - v18",
+        url: "./data/single/悲傷樂樂-v18.json",
+        video_url: "",
+        structure_version: "json_structure_18.json", // 指派新的結構檔
+    },
+
+    {
+        fileName: "室內純樂器演奏-v18.json",
+        fileDisplayName: "室內純樂器演奏 - v18",
+        url: "./data/single/室內純樂器演奏-v18.json",
+        video_url: "",
+        structure_version: "json_structure_18.json", // 指派新的結構檔
+    },
+
+    {
+        fileName: "竹塹國樂節傳統國樂演奏-v18.json",
+        fileDisplayName: "竹塹國樂節傳統國樂演奏 (只給 mp3)-v18",
+        url: "./data/single/竹塹國樂節傳統國樂演奏-v18.json",
+        video_url: "",
+        structure_version: "json_structure_18.json", // 指派新的結構檔
+    },
+
+    {
+        fileName: "khshu_-20250227210505-908662271530878-v19.json",
+        fileDisplayName: "千千 - 直播業配西堤牛排 - v19",
+        url: "./data/single/khshu_-20250227210505-908662271530878-v19.json",
+        video_url: "",
+        structure_version: "json_structure_19.json", // 指派新的結構檔
+    },
+
+    {
+        fileName: "achusan0817-20260121110932-1449050630347015-v19.json",
+        fileDisplayName: "阿啾 - 第一次交往(圖文、bgm) - v19",
+        url: "./data/single/achusan0817-20260121110932-1449050630347015-v19.json",
+        video_url: "",
+        structure_version: "json_structure_19.json", // 指派新的結構檔
+    },
+
+    {
+        fileName: "5inana-20250701123040-937232652216330-v19.json",
+        fileDisplayName: "星光奈奈 - 歷史很重要(戶外、沙啞、無bgm) - v19",
+        url: "./data/single/5inana-20250701123040-937232652216330-v19.json",
+        video_url: "",
+        structure_version: "json_structure_19.json", // 指派新的結構檔
     },
 ];
